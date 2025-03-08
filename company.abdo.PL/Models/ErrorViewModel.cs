414398140cf396cee51eca50340e049a6cf608dd
Abdelrahman <Abdelrahman@ABDOGERMANY>
2025-03-08 15:52:26 +0200

    Add project files.

---
+++ company.abdo.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace company.abdo.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
